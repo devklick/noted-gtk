96435248ee5116b99fe16c1159d8fe5407dc193d
devklick <devklick@gmail.com>
2025-11-09 20:25:17 +0000

    fix: relative paths in scripts

--- scripts/post-build.mjs
+++ scripts/post-build.mjs
@@ -1,8 +1,12 @@
 import fs from 'node:fs/promises';
 import path from 'node:path'
+import { fileURLToPath } from 'node:url';
 
-const buildAssetsDir = path.resolve('../build-assets');
-const distDir = path.resolve('../dist');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const buildAssetsDir = path.resolve(__dirname, "../build-assets");
+const distDir = path.resolve(__dirname, "../dist");
 
 
 // Copy files from build-assets/ to dist/
@@ -13,5 +17,3 @@ for(const entry of await fs.readdir(buildAssetsDir)) {
     const newEntryPath = path.join(distDir, entry);
     await fs.copyFile(entryPath, newEntryPath);
 }
-
-
