c795429ec8345f9ed64777136f512c6b1e7c268f
devklick <devklick@gmail.com>
2025-11-09 20:32:23 +0000

    Fix: prepare script

--- release.config.mjs
+++ release.config.mjs
@@ -24,7 +24,7 @@ export default {
     ],
     prepare: [
         {
-            path: "./scripts/update-meson-version.js",
+            path: "./scripts/prepare/update-meson-version.mjs",
         },
     ],
 };
